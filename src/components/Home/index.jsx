b0304b3f3d900c3eaa10751a317b2626730da717
matheustkaczyk <matheus111904@hotmail.com>
2023-05-07 11:39:30 -0300

    feat: aligning the button to the left

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -4,22 +4,23 @@ import me from '../../assets/images/me.png';
 
 function Home() {
   return (
-    <section className="w-screen h-screen flex justify-center items-center bg-yellow-400">
-      <div className='w-3/5 text-center font-mono text-5xl leading-snug text-yellow-50'>
+    <section className="h-screen flex justify-center items-center">
+      <div className='text-center w-fit font-mono text-5xl leading-snug text-yellow-50'>
         <h1>Hello!</h1>
         <h1>I'm Matheus Tkaczyk</h1>
         <h4>Fullstack developer</h4>
-        <a href="https://www.linkedin.com/in/matheustkaczykribeiro/" target='__blank' class="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded text-2xl">
-          Connect with me!
-        </a>
+        <div className='flex justify-start mt-5'>
+          <a href="https://www.linkedin.com/in/matheustkaczykribeiro/" target='__blank' class="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded text-2xl">
+            Connect with me!
+          </a>
+        </div>
       </div>
       <div>
-
       </div>
-      <div className='w-2/5 mr-10'>
+      <div className='w-2/5 ml-20'>
         <img className='rounded-full w-fit border-amber-400 border-x-2' src={me} alt="Matheus Tkaczyk" />
       </div>
-      <AiOutlineArrowDown className='text-5xl text-yellow-50 absolute bottom-5 animate-bounce text-black' />
+      <AiOutlineArrowDown className='text-5xl absolute bottom-5 animate-bounce text-black cursor-pointer' />
     </section>
   );
 }
