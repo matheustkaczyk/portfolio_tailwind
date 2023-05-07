e4c4c4f5d09afd3c58e6a1ccd28fded2cf9d0b70
matheustkaczyk <matheus111904@hotmail.com>
2023-05-07 10:19:39 -0300

    feat: creating basic header structure

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,12 @@
+import linkedinIcon from '../../assets/images/linkedin.png'
+import githubIcon from '../../assets/images/github.png'
+
+function Header() {
+  return (
+    <header className='flex flex-row items-center justify-around mx-auto h-auto w-screen p-5 border-b-4'>
+      <h1 className="font-mono font-thin text-2xl">Tkaczyk</h1>
+    </header>
+  )
+}
+
+export default Header
