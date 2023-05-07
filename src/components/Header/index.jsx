d0dee65b4429a83f0fc0e44c8c0e2d302621ffa0
matheustkaczyk <matheus111904@hotmail.com>
2023-05-07 10:25:51 -0300

    feat: changing header color

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,7 +3,7 @@ import githubIcon from '../../assets/images/github.png'
 
 function Header() {
   return (
-    <header className='flex flex-row items-center justify-around mx-auto h-auto w-screen p-5 border-b-4'>
+    <header className='flex flex-row items-center justify-around mx-auto h-auto w-screen p-5 border-b-2 bg-yellow-400'>
       <h1 className="font-mono font-thin text-2xl">Tkaczyk</h1>
     </header>
   )
