44f6e2016862937e08134c72d850ff3d3ee69be0
matheustkaczyk <matheus111904@hotmail.com>
2023-05-07 11:34:28 -0300

    feat: added ultrawide constraints

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,15 @@ import Home from "./components/Home"
 function App() {
 
   return (
-    <div className="App">
-      <Header />
-      <Home />
+    <div className="App bg-yellow-400">
+      <div className="flex justify-center">
+        <div className="max-w-7xl">
+          <Header />
+          <Home />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default App
