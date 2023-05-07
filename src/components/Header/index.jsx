453014070329722a195f5b3ec1090e11ea333f8a
matheustkaczyk <matheus111904@hotmail.com>
2023-05-07 10:47:54 -0300

    feat: adding logic to change header look on scroll

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,31 @@
-import linkedinIcon from '../../assets/images/linkedin.png'
-import githubIcon from '../../assets/images/github.png'
+import { useState, useEffect } from 'react';
+import { GiHamburgerMenu } from 'react-icons/gi';
 
 function Header() {
+  const [isSticky, setIsSticky] = useState(false);
+
+  useEffect(() => {
+    function handleScroll() {
+      if (window.pageYOffset > 0) {
+        setIsSticky(true);
+      } else {
+        setIsSticky(false);
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <header className='flex flex-row items-center justify-around mx-auto h-auto w-screen p-5 border-b-2 bg-yellow-400'>
-      <h1 className="font-mono font-thin text-2xl">Tkaczyk</h1>
+    <header className={`fixed mx-auto h-auto w-screen p-5 border-b-2 bg-yellow-400 top-0 left-0 transition-all duration-500 ease-in-out z-50 ${isSticky ? 'h-12' : 'h-16'}`}>
+      <div className='flex flex-row justify-between items-center'>
+        <h1 className="font-mono font-thin text-2xl">Tkaczyk</h1>
+        <GiHamburgerMenu className='text-2xl' />
+      </div>
     </header>
   )
 }
