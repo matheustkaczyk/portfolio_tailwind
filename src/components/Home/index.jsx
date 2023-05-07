c3e5cfd9943c03c6607e472db6220df928048bc0
matheustkaczyk <matheus111904@hotmail.com>
2023-05-07 10:25:57 -0300

    feat: adding linkedin button

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -7,6 +7,12 @@ function Home() {
         <h1>Hello!</h1>
         <h1>I'm Matheus Tkaczyk</h1>
         <h4>Fullstack developer</h4>
+        <a href="https://www.linkedin.com/in/matheustkaczykribeiro/" target='__blank' class="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded text-2xl">
+          Connect with me!
+        </a>
+      </div>
+      <div>
+
       </div>
       <div className='w-2/5'>
         <img className='rounded-full w-fit border-amber-400 border-x-2' src={me} alt="Matheus Tkaczyk" />
