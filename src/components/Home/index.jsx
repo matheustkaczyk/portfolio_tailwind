d4dcf22d41b6343d58679d8f2243eaead86d9ab7
matheustkaczyk <matheus111904@hotmail.com>
2023-05-08 14:33:00 -0300

    feat: added button

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -9,9 +9,12 @@ function Home() {
         <h1>Hello!</h1>
         <h1>I'm Matheus Tkaczyk</h1>
         <h4>Fullstack developer</h4>
-        <div className='flex justify-start mt-5'>
-          <a href="https://www.linkedin.com/in/matheustkaczykribeiro/" target='__blank' class="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded text-2xl">
+        <div className='flex justify-start mt-5 text-3xl'>
+          <a href="https://www.linkedin.com/in/matheustkaczykribeiro/" target='__blank' className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded text-2xl">
             Connect with me!
+          </a>
+          <a href="https://github.com/matheustkaczyk" target="_blank" rel="noopener noreferrer" className="bg-gray-800 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded ml-2">
+            Go to GitHub
           </a>
         </div>
       </div>
