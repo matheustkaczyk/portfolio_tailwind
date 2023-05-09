1a84bbd62ae0088f337167acffb6e2b6dd9caf10
matheustkaczyk <matheus111904@hotmail.com>
2023-05-09 11:35:41 -0300

    feat: created basic services section

---
+++ src/components/Services/index.jsx
@@ -0,0 +1,9 @@
+function Services() {
+  return (
+    <section id="services" className="w-full h-screen mt-40">
+      <h1>How can i help you?</h1>
+    </section>
+  )
+}
+
+export default Services;
