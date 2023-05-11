113dd17336489cebbd55021e2d340198f0f1f9a9
matheustkaczyk <matheus111904@hotmail.com>
2023-05-11 12:51:02 -0300

    feat: changing font on textcard

--- src/components/TextCard/index.jsx
+++ src/components/TextCard/index.jsx
@@ -1,6 +1,6 @@
 function TextCard({ title, text, onHover, onLeave }) {
   return (
-    <div className="rounded-lg shadow-lg bg-white dark:bg-yellow-300 mx-4 my-6 max-w-sm w-ful transition duration-300 transform hover:-translate-y-1 hover:shadow-2xl hover:bg-yellow-400" onMouseEnter={onHover} onMouseLeave={onLeave}>
+    <div className="rounded-lg shadow-lg bg-white dark:bg-yellow-300 mx-4 my-6 max-w-sm w-ful transition duration-300 transform hover:-translate-y-1 hover:shadow-2xl hover:bg-yellow-400 font-semibold" onMouseEnter={onHover} onMouseLeave={onLeave}>
       <div className="p-4">
         <h3 className="font-semibold text-lg">{title}</h3>
         <p className="text-gray-600 dark:text-black mt-2">{text}</p>
